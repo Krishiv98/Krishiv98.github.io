fbed5878a54dc1f6581fdd3c381b520f0b131ada
Soni <soni0931@saskpolytech.ca>
2023-06-19 21:42:36 -0600

    Updated about and project contents

--- my-portfolio/src/views/About.js
+++ my-portfolio/src/views/About.js
@@ -21,14 +21,12 @@ const About = () => {
             </Col>
             <Col xs={12} md={6}>
               <Row className=" align-items-start p-2 my-details rounded tex align-text-left " >
-                <br />Aspiring Full-Stack Developer and Devops Engineer with expertise in React.js, Vue.js, Microsoft Azure, Three js, Scripting languages like Bash, Powershell and Perl. Currently learning React Native for Mobile Application.
+                <br />Aspiring Full-Stack Developer and Devops Engineer with expertise in React.js, Vue.js, Microsoft Azure, Three js, Scripting languages like Bash, Powershell and Perl. Currently learning Machine learning and Data Analytics using Python, TensorFlow, and Flask at my current job.
                 <br /><br/>
-                I am a passionate programmer and a learner,
-                 born and brought up in India. Currently,
-                  I am enrolled in Computer systems Technology at Saskatchewan Polytechnic, Saskatoon.
+                I am a passionate programmer and a learner,a recent graduate of Computer Systems Technology at Saskatchewan Polytechnic, Saskatoon.
                 <br />
                 <br />
-                Along with that, I an International Student Recuirment Assistant at Saskatchewan Polytechnic and a freelancer Excel Automation specialist. 
+                Along with that, I an Research Assistant and International Student Recuirment Assistant at Saskatchewan Polytechnic. In my free time I am a freelancer Excel Automation specialist on Fiverr. 
                 <br />I love learning about new technologies, what problems are they solving and How can I use them to build better and scalable products.                
                 <br /> <br />
                 <Col className="d-flex justify-content-center flex-wrap">
